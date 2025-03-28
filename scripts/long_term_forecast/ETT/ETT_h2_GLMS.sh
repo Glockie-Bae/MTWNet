93dc2507fc1eef4a16649d7ec9aa3d5fb5a22a18
Glockie-Bae <84165589+Glockie-Bae@users.noreply.github.com>
2025-03-28 16:57:30 +0800

    Add files via upload

---
+++ scripts/long_term_forecast/ETT/ETT_h2_GLMS.sh
@@ -0,0 +1,43 @@
+python -u train.py \
+  --task_name long_term_forecast \
+  --root_path ./dataset/ \
+  --data_path ETT-small/ETTh2.csv \
+  --data ETTh2 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+
+
+python -u train.py \
+  --task_name long_term_forecast \
+  --root_path ./dataset/ \
+  --data_path  ETT-small/ETTh2.csv \
+  --data ETTh2 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 192 \
+
+
+  python -u train.py \
+  --task_name long_term_forecast \
+  --root_path ./dataset/ \
+  --data_path  ETT-small/ETTh2.csv \
+  --data ETTh2 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 336 \
+
+
+  python -u train.py \
+  --task_name long_term_forecast \
+  --root_path ./dataset/ \
+  --data_path  ETT-small/ETTh2.csv \
+  --data ETTh2 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+
