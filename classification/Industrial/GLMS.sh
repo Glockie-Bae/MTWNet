5847c6a6747b5631d9524312f735b6b75e12cd78
Glockie-Bae <84165589+Glockie-Bae@users.noreply.github.com>
2025-09-07 16:25:21 +0800

    Delete classification directory

--- classification/Industrial/GLMS.sh
+++
@@ -1,5 +0,0 @@
-python -u train.py \
-  --task_name classification \
-  --public_data false \
-  --dataset ../DotDataset/DotDateset/all/  \
-  --epoch_count 30 \
--- classification/public_data/GLMS.sh
+++
@@ -1,5 +0,0 @@
-python -u train.py \
-  --task_name classification \
-  --public_data true \
-  --dataset_path ./public_dataset/Multivariate_ts/ \
-  --epoch_count 30 \
